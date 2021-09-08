41fa8787f5f6ea4c342aac731d4e56c8976581b1
Aurelien Schaegis <schaegis5u@etu.univ-lorraine.fr>
2021-09-08 17:50:49 +0200

    1ere page

---
+++ src/Controller/AppController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Doctrine\Inflector\Rules\Transformation;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class AppController extends AbstractController{
+    /**
+     * @Route("/")
+     */
+    public function home(): Response
+    {
+        return $this->render("app/home.html.twig");
+    }
+}
+
+?>
